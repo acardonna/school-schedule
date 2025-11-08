23893c928c020dd930a3e7f3ab1173caa96687f8
Alejandro Cardona <alejo.98k@gmail.com>
2025-11-07 23:31:22 -0500

    Add Teacher model

--- src/main/java/com/solvd/schoolschedule/model/Teacher.java
+++ src/main/java/com/solvd/schoolschedule/model/Teacher.java
@@ -1,6 +1,30 @@
 package com.solvd.schoolschedule.model;
 
 public class Teacher {
-    private String Name;
-    private String subject;
+    private final int id;
+    private final String name;
+    private final Subject subject;
+
+    public Teacher(int id, String name, Subject subject) {
+        this.id = id;
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + subject.getDisplayName() + ")";
+    }
 }
